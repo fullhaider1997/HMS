7495bb255b4c6ee0e4fcab8a712002e327ff6b53
Haider <haideralhafiz@gmail.com>
2020-11-04 20:44:46 -0500

    Restructure of software project

--- src/Controllers/AdminController.java
+++ src/Controllers/AdminController.java
@@ -6,8 +6,12 @@
 package Controllers;
 
 import java.io.IOException;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -23,28 +27,39 @@ import javafx.stage.Stage;
  */
 public class AdminController implements Initializable {
 
-    
+      private static final String SERVER_IP = "127.0.0.1";
+      private static final int SERVER_PORT = 9090;
     
     public void ButtonGoToLogin(ActionEvent event) throws IOException{
         
-        System.out.println("Hello world");
+        
         Parent parentScene = FXMLLoader.load(getClass().getResource("/Usergui/FXMLLoginSystem.fxml"));
-        
         Scene NextScene = new Scene(parentScene);
-        
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        
         window.setScene(NextScene);
         window.show();
     }    
+    public void EmployeeTab(ActionEvent event) throws IOException{
+        
+        System.out.println("Employee tab");
+    }
     
     
-    /**
-     * Initializes the controller class.
-     */
+    
+   
+    
+    public static void Main(String[] args) throws IOException {
+         System.out.println("Admin controller");
+                
+         
+   
+    }
+
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void initialize(URL location, ResourceBundle resources) {
+        {
+        }
+        }
+
     
 }
