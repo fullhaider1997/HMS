963dcfe50881f1f1575c073ca69e5a249a9e8fa3
Haider <haideralhafiz@gmail.com>
2020-11-08 15:21:00 -0500

    Removed pervious server files.

--- src/Controllers/HosptialEmployeesController.java
+++ src/Controllers/HosptialEmployeesController.java
@@ -1,27 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author Haider
- */
-public class HosptialEmployeesController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+public class HosptialEmployeesController implements Initializable {
+    
+     
+    
+   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        
+        
+        
     }    
     
 }
